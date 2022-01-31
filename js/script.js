511fc93493ea140d2f2f6a1d468c13c85ef9d93b
MadDev3 <vmakeev.dev@gmail.com>
2022-01-31 23:20:11 +0400

    syntax

--- js/script.js
+++ js/script.js
@@ -2,7 +2,7 @@
 var bl = "none";
 
 function ShowMobileMenu() {
-	var block = document.getElementById('downMenu');
+	var block = document.getElementById('down-menu');
 	if (bl=="none") {
 		block.style.display = "block";
 		document.getElementById('burger').innerHTML = "&times;";
